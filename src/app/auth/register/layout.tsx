ee36f1d93ae10aef2b2e628c93c533255f8007c2
Vova4o <gavrilenko2012@gmail.com>
2026-01-07 21:22:45 +0300

    fix: format description in metadata for consistency

--- src/app/auth/register/layout.tsx
+++ src/app/auth/register/layout.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Регистрация",
-  description: "Создайте аккаунт для доступа к урокам и материалам по программированию на Go.",
+  description:
+    "Создайте аккаунт для доступа к урокам и материалам по программированию на Go.",
 };
 
 export default function RegisterLayout({
